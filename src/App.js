cf8fb9d559d2f61c550023079d124cfa792e9919
nghiaphan <phannghia10201a@gmail.com>
2022-08-29 13:53:28 +0700

    update fun2

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React with My~ ngu
+          Learn React with My~ ngu1
         </a>
       </header>
     </div>
